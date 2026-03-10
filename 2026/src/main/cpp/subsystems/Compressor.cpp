3b7a613eee0bf2c0195768d1f8ebab8a80e774a2
Noah Carter <woe6933@gmail.com>
2026-03-10 08:35:52 -0500

    removing compressor class

--- 2026/src/main/cpp/subsystems/Compressor.cpp
+++
@@ -1,7 +0,0 @@
-#include "subsystems/Compressor.h"
-
-Compressor::Compressor()
-{
-  pnH.EnableCompressorAnalog( units::pounds_per_square_inch_t{80}, //Min On Pressure
-                              units::pounds_per_square_inch_t{95}); //Max Off Pressure
-}
